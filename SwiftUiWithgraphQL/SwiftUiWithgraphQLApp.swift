8d987feeb4c42a0bf6a86bdd420d9f799d8aea50
Sunil Gurung <sunil@prezzee.com.au>
2022-10-29 10:08:51 +0800

    - x - Added all the refresh install

---
+++ SwiftUiWithgraphQL/SwiftUiWithgraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiWithgraphQLApp.swift
+//  SwiftUiWithgraphQL
+//
+//  Created by Sunil Gurung on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiWithgraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
